120b132097bbd0bd51f8ffe87f5eac8cbe874c9c
Self-taught <63093667+Self-taught@users.noreply.github.com>
2023-03-17 21:36:32 +0530

    Added SQLlite database to store the data of events.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 import selectorlib
 from emailing import send_email
+import time
+import sqlite3
 
+connection = sqlite3.connect("data.db")
 URL = "http://programmer100.pythonanywhere.com/tours/"
 
 
@@ -19,20 +22,31 @@ def extract(source):
 
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 
-def read():
-    with open("data.txt", "r") as file:
-        return file.read()
+def read(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
-if __name__ == "__main__":
-    scraped = scrape(URL)
-    exctracted = extract(scraped)
-    content = read()
-    print(exctracted)
-    if exctracted != "No upcoming tours":
-        if exctracted not in content:
-            store(exctracted)
-            send_email(exctracted)
+while True:
+    if __name__ == "__main__":
+        scraped = scrape(URL)
+        exctracted = extract(scraped)
+        print(exctracted)
+        if exctracted != "No upcoming tours":
+            row = read(exctracted)
+            if not row:
+                store(exctracted)
+                send_email(exctracted)
+        time.sleep(3)
